308d4ca6ada25ea09e005f76198089b567d1251f
an_ttt_a4h20 <406054225@qq.com>
2026-02-28 23:19:40 +0800

    android

---
+++ include/android/android_config.h
@@ -0,0 +1,13 @@
+#ifndef ANDROID_CONFIG_WRAPPER_H
+#define ANDROID_CONFIG_WRAPPER_H
+
+#include "config.h"
+
+#include <unistd.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <poll.h>
+
+#endif
